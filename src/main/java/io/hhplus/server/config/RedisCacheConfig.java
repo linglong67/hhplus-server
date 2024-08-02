cb82d5cac3a49ff01391573ee58c92a59c1662ff
Younglong <kyl8555@gmail.com>
2024-08-02 21:52:45 +0900

    feat: jackson, serializer 관련 추가

--- src/main/java/io/hhplus/server/config/RedisCacheConfig.java
+++ src/main/java/io/hhplus/server/config/RedisCacheConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
@@ -23,7 +23,7 @@ public class RedisCacheConfig {
                 .serializeKeysWith(RedisSerializationContext.SerializationPair
                         .fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                        .fromSerializer(RedisSerializer.java()))
                 .entryTtl(Duration.ofMinutes(10L));
 
         return RedisCacheManager.RedisCacheManagerBuilder
--- src/main/java/io/hhplus/server/domain/concert/ConcertSchedule.java
+++ src/main/java/io/hhplus/server/domain/concert/ConcertSchedule.java
@@ -1,15 +1,23 @@
 package io.hhplus.server.domain.concert;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
-public class ConcertSchedule {
+public class ConcertSchedule implements Serializable {
     private Long id;
     private Long concertId;
     private Long placeId;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime concertDatetime;
 }
